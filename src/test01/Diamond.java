9f0f552554a7db517ef946e8bf2721388eb5c733
cagla88 <caglaataseven1988@gmail.com>
2023-07-05 14:35:19 +0300

    added Diamond class

---
+++ src/test01/Diamond.java
@@ -0,0 +1,22 @@
+package test01;
+
+public class Diamond {
+
+    public static void main(String[] args) {
+
+        System.out.println("\t\t\t     ^ ");
+        System.out.println("\t\t\t    / \\");
+        System.out.println("\t\t\t   / * \\");
+        System.out.println("\t\t\t  / * * \\");
+        System.out.println("\t\t\t / * * * \\");
+        System.out.println("\t\t\t ---------");
+        System.out.println("\t\t\t \\ * * * /");
+        System.out.println("\t\t\t  \\ * * /");
+        System.out.println("\t\t\t   \\ * /");
+        System.out.println("\t\t\t    \\ /");
+        System.out.println("\t\t\t     v");
+
+    }
+
+
+}
